8a85948cafba793f8294868a6bcc207a1b0536af
Danielqwf <sheidhead@gmail.com>
2023-06-08 23:34:06 +0300

    gfg

--- custom_users/forms.py
+++ custom_users/forms.py
@@ -7,19 +7,32 @@ GENDER = (
     ('Ж', 'Ж'),
 )
 
+MARRIED = (
+    ('Да', 'Да'),
+    ('Нет', 'Нет'),
+)
+
+
 class CustomRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    phone_number = forms.CharField(max_length=100, required=True)
+    country = forms.CharField(max_length=100, required=True)
+    region = forms.CharField(max_length=100, required=True)
+    area = forms.CharField(max_length=100, required=True)
+    city = forms.CharField(max_length=50, required=True)
+    street = forms.CharField(max_length=40, required=True)
+    house = forms.CharField(max_length=30, required=True)
+    phone_number = forms.IntegerField(required=True)
     age = forms.IntegerField(required=True)
-    gender = forms.ChoiceField(required=True, choices=GENDER)
-    work = forms.CharField(max_length=100)
+    gender = forms.ChoiceField(choices=GENDER, required=True)
+    married = forms.ChoiceField(choices=MARRIED, required=True)
 
 
     class Meta:
         model = models.CustomUsers
         fields = (
             'username', 'email', 'password1', 'password2', 'first_name',
-            'last_name', 'age', 'gender', 'phone_number', 'work'
+            'last_name', 'country', 'region', 'area', 'city', 'street', 'house', 'phone_number',
+            'age', 'gender', 'married'
         )
 
     def save(self, commit=True):
--- custom_users/models.py
+++ custom_users/models.py
@@ -7,16 +7,29 @@ GENDER = (
     ('Ж', 'Ж'),
 )
 
+MARRIED = (
+    ('Да', 'Да'),
+    ('Нет', 'Нет'),
+)
 
 class CustomUsers(User):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
 
+    country = models.CharField(max_length=100, null=True)
+    region = models.CharField(max_length=100, null=True)
+    area = models.CharField(max_length=100, null=True)
+    city = models.CharField(max_length=50, null=True)
+    street = models.CharField(max_length=40, null=True)
+    house = models.CharField(max_length=30, null=True)
     phone_number = models.CharField(max_length=13, default='+996')
     age = models.PositiveIntegerField(default=16)
     gender = models.CharField(max_length=3, choices=GENDER)
-    work = models.TextField(blank=True, null=True)
+    about = models.TextField(max_length=100, blank=True, null=True)
+    married = models.CharField(max_length=3, blank=True, null=True, choices=MARRIED)
+
+
 
 
     def __str__(self):
